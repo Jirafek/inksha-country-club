d5c67a53054af8cd5aa403bc63fd5b826f7d728c
Serafim Nikolaev <86237844+Jirafek@users.noreply.github.com>
2023-08-03 15:25:49 +0300

    Delete node_modules directory

--- node_modules/air-datepicker/locale/el.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'air-datepicker/locale/el' {
-    import {AirDatepickerLocale} from 'air-datepicker';
-    const el: AirDatepickerLocale;
-
-    export default el;
-}
--- node_modules/air-datepicker/locale/id.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'air-datepicker/locale/id' {
-    import {AirDatepickerLocale} from 'air-datepicker';
-    const id: AirDatepickerLocale;
-
-    export default id;
-}

a95303022506f4a56689af9d5ef7fcf3ea7570f0
SerafimNikalayeu <serafim.nikalayeu@digitalinspirationlabs.io>
2023-08-03 17:53:56 +0300

    feat: 404

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -5,8 +5,8 @@ import { Link } from "react-router-dom";
 
 const NotFound = () => {
     return (
-        <section style={{ backgroundImage: 'url(/image/404.webp)' }} className="relative h-[851px] back_settings">
-            <Link to="/#book" className="back_settings absolute top-[17rem] left-1/2 w-[302px] h-[63px] flex justify-center items-center monterey text-white text-[18px]" style={{ backgroundImage: 'url(/image/404_btn.webp)', transform: 'translateX(-50%)' }}>
+        <section style={{ backgroundImage: 'url(/image/404.webp)', backgroundSize: 'contain' }} className="relative h-[851px] back_settings bg-[#bdc7ed]">
+            <Link to="/#book" className="back_settings absolute top-[18rem] left-1/2 w-[302px] h-[63px] flex justify-center items-center monterey text-white text-[18px]" style={{ backgroundImage: 'url(/image/404_btn.webp)', transform: 'translateX(-50%)' }}>
                 Вернуться на главную
             </Link>
         </section>

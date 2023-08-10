66f18c59e1a91f4af38304e32730112f2b048b56
kubanmedia <kubanmedia@pochtamt.ru>
2023-08-10 11:12:42 +0500

    Update Gallery.jsx

--- src/components/Home/Gallery.jsx
+++ src/components/Home/Gallery.jsx
@@ -18,7 +18,7 @@ const Gallery = () => {
         <section className="about_background back_settings relative" id="gallery">
             <div className="flex flex-col items-center justify-center relative h-[90px] z-10">
                 <img className="absolute" src="/image/titles_bg.webp" alt="" />
-                <h1 className="text-[20px] text-white z-10 font-bold">ГАЛЕРЕЯ</h1>
+                <h2 className="text-[20px] text-white z-10 font-bold">ГАЛЕРЕЯ</h2>
             </div>
             <GallerySlider images={images} />
             <img className="absolute -bottom-[2px] w-full" src="/image/gallery_translate_layer.webp" alt="" />

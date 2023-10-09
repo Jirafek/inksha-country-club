42d9cbb991063dbf58aa8acd275879446ba2310a
SerafimNikalayeu <serafim.nikalayeu@digitalinspirationlabs.io>
2023-10-10 00:37:18 +0300

    fix: del: pagination

--- src/components/Home/helpers/Slider.jsx
+++ src/components/Home/helpers/Slider.jsx
@@ -1,44 +1,35 @@
-import React from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import SwiperCore from "swiper";
-import { Pagination } from "swiper/modules";
-
-SwiperCore.use([Pagination]);
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore from 'swiper';
 
 const GallerySlider = ({ images }) => {
-   return (
-      <Swiper
-         spaceBetween={0}
-         slidesPerView={1}
-         direction="vertical"
-         pagination={{ clickable: true }}
-         style={{ width: "100%", height: "600px" }}
-         autoplay={{ delay: 1000 }}
-         onInit={(swiper) => {
-            swiper.slides.forEach((slide, index) => {
-               const angle = -45 * index; // Угол поворота слайда
-               slide.style.transform = `rotate(${angle}deg) translateY(-50%)`;
-               slide.style.transformOrigin = "50% 50%"; // Задаем точку применения поворота по центру слайда
-            });
-         }}
-      >
-         {images.map(({ avif, webp }, index) => (
-            <SwiperSlide
-               key={index}
-               className="flex h-full items-center justify-center"
-            >
-               <picture>
-                  <source srcSet={`${avif} 1x`} type="image/avif" />
-                  <img
-                     src={webp}
-                     alt={`Слайд №${index + 1}`}
-                     className="max-h-full max-w-full -rotate-45 transform"
-                  />
-               </picture>
-            </SwiperSlide>
-         ))}
-      </Swiper>
-   );
+  return (
+    <Swiper
+      spaceBetween={0}
+      slidesPerView={1}
+      direction="vertical"
+      pagination={{ clickable: true }}
+      style={{ width: '100%', height: '600px' }}
+      autoplay={{ delay: 1000 }}
+      onInit={(swiper) => {
+        swiper.slides.forEach((slide, index) => {
+          const angle = -45 * index; // Угол поворота слайда
+          slide.style.transform = `rotate(${angle}deg) translateY(-50%)`;
+          slide.style.transformOrigin = '50% 50%'; // Задаем точку применения поворота по центру слайда
+        });
+      }}
+    >
+      {images.map(({ avif, webp }, index) => (
+        <SwiperSlide key={index} className="flex items-center justify-center h-full">
+            <picture>
+                <source srcSet={`${avif} 1x`} type="image/avif" />
+                <img src={webp} alt={`Слайд №${index + 1}`} className="max-w-full max-h-full transform -rotate-45" />
+            </picture>
+
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
 };
 
 export default GallerySlider;
